20b2ab3dac97a324c421e3b0b6a3e02561d86a7e
Gaerl <gqueiroz.calado@gmail.com>
2023-05-03 14:02:48 -0300

    atualização de filas

---
+++ Fila/src/one/digitalinovation/Fila.java
@@ -0,0 +1,89 @@
+package one.digitalinovation;
+
+public class Fila {
+
+    private No refNoEntradaFila;
+    //Referência de entrada pra fila, entrando pelo final dela
+
+    public Fila() {
+        this.refNoEntradaFila = null;
+        //Estancio a fila vazia
+
+    }
+
+
+    //Métodos enqueue, dequeue e isEmpty
+    public void enqueue(No novoNo) {
+        novoNo.setRefNo(refNoEntradaFila);
+        refNoEntradaFila = novoNo;
+
+        //Recebi um novo nó, o novo nó entrou na fila, a referencia de novo nó era o antigo ultimo nó da fila
+        //Agora ele vai referenciar o penultimo nó e esse novo nó será o ultimo nó da fila
+
+    }
+
+    public No first() {
+        if (!this.isEmpty()) {
+            No primeiroNo = refNoEntradaFila;
+            while (true) {
+                if (primeiroNo.getRefNo() != null) {
+                    primeiroNo = primeiroNo.getRefNo();
+
+                } else {
+                    break;
+                }
+            }
+            return primeiroNo;
+        }
+        return null;
+    }
+
+    public No dequeue() {
+        if (!this.isEmpty()) {
+            No primeiroNo = refNoEntradaFila;
+            No noAuxiliar = refNoEntradaFila;
+            while (true) {
+                if (primeiroNo.getRefNo() != null) {
+                    noAuxiliar = primeiroNo;
+                    primeiroNo = primeiroNo.getRefNo();
+
+                } else {
+                    noAuxiliar.setRefNo(null);
+                    break;
+                }
+            }
+            return primeiroNo;
+        }
+        return null;
+    }
+
+
+    public boolean isEmpty() {
+        return refNoEntradaFila == null ? true : false;
+        //Método que analisa saber se a fila esta vazia
+        //Se a referencia do (refNoEntradaFila) No for igual a null ela estará vazia.
+
+    }
+
+    @Override
+    public String toString() {
+        String stringRetorno = "";
+        No noAuxiliar = refNoEntradaFila;
+        if (refNoEntradaFila != null) {
+            while (true) {
+                stringRetorno += "[No{objeto=" + noAuxiliar.getObject() + "}]--->";
+                if (noAuxiliar.getRefNo() != null) {
+                    noAuxiliar = noAuxiliar.getRefNo();
+                } else {
+                    stringRetorno += "null";
+                    break;
+                }
+            }
+        } else {
+            stringRetorno = "null";
+        }
+
+        return stringRetorno;
+    }
+
+}
